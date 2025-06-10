fb42b027498e3775b34407abb993eba05b7e2c17
Mirack <nwadiaromiraclechukwuma@gmail.com>
2025-06-10 17:50:27 +0100

    first commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+} from "react-native";
 
 export default function App() {
+  const [text, setText] = React.useState("");
+
+  const handleConvert = () => {};
+
   return (
     <View style={styles.container}>
-      <Text>I am here</Text>
+      <TextInput
+        value={text}
+        placeholder="Enter text"
+        onChangeText={setText}
+        style={styles.input}
+        multiline
+      />
+      <TouchableOpacity onPress={handleConvert} style={styles.button}>
+        <Text style={styles.buttonText}>Convert to audio</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -12,7 +32,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 10,
+    gap: 10,
+    paddingTop: 50,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "gainsboro",
+    borderRadius: 10,
+    padding: 10,
+    minHeight: 50,
+    textAlignVertical: "top",
+  },
+  button: {
+    backgroundColor: "royalblue",
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 50,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontWeight: "600",
   },
 });
