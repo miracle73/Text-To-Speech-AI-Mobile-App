68d5c0b389296717a2968791885ea7170bd6e851
Mirack <nwadiaromiraclechukwuma@gmail.com>
2025-06-23 04:01:22 +0100

    Added ElevenLabs to the Project

--- src/app/api/tts+api.ts
+++ src/app/api/tts+api.ts
@@ -1,8 +1,44 @@
+import { ElevenLabsClient } from "elevenlabs";
+
+const client = new ElevenLabsClient({
+  apiKey: process.env.ELEVENLABS_API_KEY,
+});
+
 export function GET(request: Request) {
   return Response.json({ hello: "world" });
 }
 
 export async function POST(request: Request) {
+  const { text } = await request.json();
+  if (!text) {
+    return Response.json(
+      {
+        error: "Text is required",
+      },
+      { status: 400 }
+    );
+  }
+
+  const audio = await client.textToSpeech.convert("", {
+    text,
+    model_id: "eleven_multilingual_v2",
+    output_format: "mp3_44100_128",
+  });
+
+  const chunks = [];
+  for await (const chunk of audio) {
+    chunks.push(chunk);
+  }
+  const audioBuffer = Buffer.concat(chunks);
+
+  return new Response(audioBuffer, {
+    headers: {
+      "Content-Type": "audio/mpeg",
+      // "Content-Length": audioBuffer.length.toString(),
+    },
+  });
+}
+export async function POST2(request: Request) {
   const { text } = await request.json();
   console.log("From the server,", text);
   if (!text) {
